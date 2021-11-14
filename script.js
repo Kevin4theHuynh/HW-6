bd56ca301ad8f9bab6eaf62c78e07230d329025d
Kevin4theHuynh <85657180+Kevin4theHuynh@users.noreply.github.com>
2021-11-14 12:10:47 -0800

    Added searchbar and image

---
+++ script.js
@@ -0,0 +1,11 @@
+var weatherAPI = 'd91f911bcf2c0f925fb6535547a5ddc9';
+var weatherUrl = 'https://api.openweathermap.org';
+
+var searchForm = document.querySelector('#search-form');
+var searchInput = document.querySelector('#search-input');
+var todayContainer = document.querySelector('#today');
+var forecastContainer = document.querySelector('#forecast');
+var searchHistoryContainer = document.querySelector('#history');
+
+
+console.log(weatherUrl)
